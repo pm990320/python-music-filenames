d0b6fe6f589d02d99e7dfbbcf7ab9d329009fbba
pm990320 <patmenlove@gmail.com>
2014-11-28 19:02:04 +0000

    ....

--- python-music-filenames.py
+++ python-music-filenames.py
@@ -1,5 +1,6 @@
 import os, shutil, string
 
+
 def copyFilesToDirectory(SOURCE_DIRECTORY, DESTINATION_DIRECTORY):
     for dirpath, dirnames, filenames in os.walk(SOURCE_DIRECTORY):
         for file in filenames:
@@ -10,6 +11,7 @@ def copyFilesToDirectory(SOURCE_DIRECTORY, DESTINATION_DIRECTORY):
             shutil.copyfile(combined_filename, DESTINATION_DIRECTORY + '/' + os.path.basename(file))
 
 
+# Example of extensions list: [ '.jpg', '.html' ]
 def removeFilesWithExtensions(DIRECTORY, EXTENSIONS_LIST):
     for root, dirnames, filenames in os.walk(DIRECTORY):
         for filename in filenames:
@@ -26,12 +28,11 @@ def renameMusicFiles(DIRECTORY):
             new_filename = filename
             #if filename[2] == " ":
                 #new_filename = filename[3:]
-            new_filename = new_filename.lstrip('0123456789')
-            new_filename = new_filename.lstrip()
-            if new_filename in filenames:
-                new_filename = "[2]" + new_filename
+            new_filename = new_filename.lstrip(' 0123456789-_')
             new_full_path = os.path.join(root, new_filename)
             old_full_path = os.path.join(root, filename)
-            print new_full_path
-            print old_full_path
+            if os.path.isfile(new_full_path):
+                new_filename = "(2)" + new_filename
+                new_full_path = os.path.join(root, new_filename)
             os.rename(old_full_path, new_full_path);
+
